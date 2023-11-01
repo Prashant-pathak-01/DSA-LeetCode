765de56837604553c02d2cc087b40245d6b3206a
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-11-02 00:04:23 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (98.46%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -1,43 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    public int counter(TreeNode root,int f, int gf){
+        if(root==null) return 0;
+        int temp = (gf%2==0)?root.val:0;
+        return counter(root.left,root.val,f)+counter(root.right,root.val,f)+temp;
+    }
     public int sumEvenGrandparent(TreeNode root) {
-        int sum = 0;
-        if(root == null || (root.left==null && root.right==null)) return sum;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode temp = q.poll();
-                boolean flag = false;
-                if(temp.val%2==0) flag = true;
-                if(temp.left!=null){
-                    if(flag && temp.left.left!=null) sum+=temp.left.left.val;
-                    if(flag && temp.left.right!=null) sum+=temp.left.right.val;
-                    q.add(temp.left);
-                }
-                if(temp.right!=null){
-                    if(flag && temp.right.left!=null) sum+=temp.right.left.val;
-                    if(flag && temp.right.right!=null) sum+=temp.right.right.val;
-                    q.add(temp.right);
-                }
-                
-            }
-        }
-        return sum;
+        if(root == null || (root.left==null && root.right==null)) return 0;
+        return counter(root,1,1);
     }
 }
