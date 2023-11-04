baa2fd470a0894dcb0c446b48d96c8713c30961b
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-11-05 01:51:47 +0530

    Time: 105 ms (11.83%) | Memory: 44.4 MB (8.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,20 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode sort(ListNode l1, ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
         ListNode res = new ListNode(0);
-        ListNode track = res;
-        List<Integer> temp = new ArrayList<>();
-        for(int i=0; i<lists.length; i++){
-            while(lists[i]!=null){
-                temp.add(lists[i].val);
-                lists[i] = lists[i].next;
+        ListNode temp = res;
+
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                res.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else{
+                res.next = new ListNode(l2.val);
+                l2 = l2.next;
             }
+            res = res.next;
+
         }
-        Collections.sort(temp);
-        for(int i=0; i<temp.size(); i++){
-            res.next = new ListNode(temp.get(i));
+        while(l1!=null){
+            res.next = new ListNode(l1.val);
+            l1 = l1.next;
             res = res.next;
         }
-        return track.next;
-        
+        while(l2!=null){
+            res.next = new ListNode(l2.val);
+            l2 = l2.next;
+            res = res.next;
+        }
+
+        return temp.next;
     }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) return null;
+        if(lists.length==1) return lists[0];
+        ListNode res = lists[0];
+        for(int i=1; i<lists.length; i++){
+            res = sort(res,lists[i]);
+        }
+        return res;
+    }
+
 }
