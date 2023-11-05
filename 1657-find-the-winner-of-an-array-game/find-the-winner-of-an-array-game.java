b77a720dbb062ae9d5b4602e1cbf949f442bdf9f
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-11-05 11:53:08 +0530

    Time: 1 ms (52.90%) | Memory: 56 MB (89.86%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -2,24 +2,19 @@ class Solution {
     public int getWinner(int[] arr, int k) {
         int prev_winner = arr[0];
         int count = 0;
-        Queue<Integer> list = new LinkedList<>();
         int max = arr[0];
         for(int i=1; i<arr.length; i++) {
-            list.add(arr[i]);
             max = Math.max(max,arr[i]);
         }
         if(k>=arr.length) return max;
         for(int i=1; i>0; i++){
-            if(prev_winner>=list.peek()){
-                int temp = list.remove();
-                list.add(temp);
+            if(prev_winner>=arr[i]){
                 count++;
             } else{
-                list.add(prev_winner);
-                prev_winner = list.remove();
+                prev_winner = arr[i];
                 count = 1;
             }
-                if(count==k) return prev_winner;
+                if(count==k || prev_winner==max) return prev_winner;
         }
         return max;
     }
