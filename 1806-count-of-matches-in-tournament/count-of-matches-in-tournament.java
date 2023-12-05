1859bccdb7460d4f438d1a10eba16d9ae72699ee
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-12-05 12:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (31.59%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int res = 0;
+        while(n>=1){
+            res+=n/2;
+            if(n==1) return res;
+            n=(n+1)/2;
+        }
+        return res;
+    }
+}
