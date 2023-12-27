d534a7285a9abdee982129914a5d5ca7d2fa655b
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-12-27 15:56:47 +0530

    Time: 8 ms (59.89%) | Memory: 45.6 MB (6.95%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -5,23 +5,23 @@ class Solution {
             set.add(nums[i]);
         }
         int count = set.size();
-        Set<Integer> temp = new HashSet<>();
+        set.clear();
         Map<Integer,Integer> map = new HashMap<>();
         int res = 0;
         int j =0;
         for(int i=0; i<nums.length; i++){
             if(map.containsKey(nums[i])){
                 map.put(nums[i],map.get(nums[i])+1);
-                temp.add(nums[i]);
+                set.add(nums[i]);
                 
             }else{
                 map.put(nums[i],1);
-                temp.add(nums[i]);
+                set.add(nums[i]);
             }
-            while(temp.size()>=count){
+            while(set.size()>=count){
                 res+=nums.length-i;
                 map.put(nums[j],map.get(nums[j])-1);
-                if(map.get(nums[j])==0) temp.remove(nums[j]);
+                if(map.get(nums[j])==0) set.remove(nums[j]);
                 j++;
             }
         }
