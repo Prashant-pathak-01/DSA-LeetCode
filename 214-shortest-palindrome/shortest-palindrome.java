4492f169592546628951bf720f46e5e3ac313729
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-01-25 23:21:24 +0530

    Time: 8 ms (58.72%) | Memory: 44.5 MB (58.09%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,19 +1,13 @@
 class Solution {
-    public boolean isPal(String s, int i, int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
     public String shortestPalindrome(String s) {
-        int i =0 ;
-       for(i=s.length()-1; i>=0 ; i--){
-           if(isPal(s.substring(0,i+1),0,i)) break;
-       }
-       StringBuilder temp = new StringBuilder(s.substring(i+1));
-       temp.reverse();
-       return temp+s;
+        if(s.length()<=1) return s;
+        String newS = s+'*'+new StringBuilder(s).reverse().toString();
+        int []pos = new int[newS.length()];
+        for(int i=1; i<newS.length(); i++){
+            int pre = pos[i-1];
+            while(pre>0 && newS.charAt(pre)!=newS.charAt(i)) pre = pos[pre-1];
+            pos[i] = pre+((newS.charAt(pre)==newS.charAt(i))?1:0);
+        }
+        return new StringBuilder(s.substring(pos[pos.length-1])).reverse().toString()+s;
     }
 }
