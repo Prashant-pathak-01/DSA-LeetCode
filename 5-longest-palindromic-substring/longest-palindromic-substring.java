294be3eef94acd564cd16479d0f5b980c67505b6
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-01-30 22:11:42 +0530

    Time: 165 ms (35.10%) | Memory: 56.6 MB (5.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,16 +1,22 @@
 class Solution {
-    public boolean isPalindrome(String str){
-        int i=0; 
-        int j= str.length()-1;
-        while(i<j) if(str.charAt(i++)!=str.charAt(j--)) return false;
-        return true;
+    public boolean isPalindrome(String str,int i, int j,int dp[][]){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j]==1;
+        if(str.charAt(i)!=str.charAt(j)){
+            dp[i][j] = 0;
+            return false;
+        }
+        dp[i][j] = isPalindrome(str,i+1,j-1,dp)?1:0;
+        return dp[i][j]==1;
     }
     public String longestPalindrome(String s) {
         int prev = 0;
         String res = "";
+        int dp[][] = new int[s.length()][s.length()];
+        for(int i[]:dp) Arrays.fill(i,-1);
         for(int i=0; i<s.length(); i++){
             for(int j=i; j<s.length(); j++){
-                if(j-i+1>prev && isPalindrome(s.substring(i,j+1))) {
+                if(j-i+1>prev && isPalindrome(s,i,j,dp)) {
                     prev = j-i+1;
                     res = s.substring(i,j+1);
                 }
