3f9c381d8e3f4476574bf9d74f3e7625bc1a9ff8
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-02-06 22:11:51 +0530

    Time: 8 ms (65.09%) | Memory: 43 MB (43.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,14 +1,21 @@
 class Solution {
+    public boolean check(int arr1[], int arr2[]){
+        for(int i=0; i<26; i++){
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
-        char arr[] = s1.toCharArray();
-        Arrays.sort(arr);
-        s1 = String.valueOf(arr);
-        for(int i=s1.length()-1; i<s2.length(); i++){
-            String temp = s2.substring(i-s1.length()+1,i+1);
-            char sub[] = temp.toCharArray();
-            Arrays.sort(sub);
-            String str = String.valueOf(sub);
-            if(str.equals(s1)) return true;
+        int a[] = new int[26];
+        for(int i=0; i<s1.length(); i++) a[s1.charAt(i)-'a']++;
+        int b[] = new int[26];
+        int j = 0;
+        for(int i=0; i<s2.length(); i++){
+            b[s2.charAt(i)-'a']++;
+            if(i<s1.length()-1) continue;
+            if(check(a,b)) return true;
+            b[s2.charAt(j)-'a']--;
+            j++;
         }
         return false;
     }
