cbd896979438da057f9b79140bec8c68e83f4c3b
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-02-12 22:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (16.42%) - LeetSync

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int res= 0;
+        for(int i=0; i<32; i++){
+            boolean aa=false,bb=false,cc=false;
+            if((a&(1<<i))!=0) aa = true;
+            if((b&(1<<i))!=0) bb = true;
+            if((c&(1<<i))!=0) cc = true;
+            
+            if(cc){
+                if(!aa && !bb) res++;
+            }else{
+                if(aa && bb) res+=2;
+                else if(aa || bb) res++;
+            }
+        }
+        return res;
+    }
+}
