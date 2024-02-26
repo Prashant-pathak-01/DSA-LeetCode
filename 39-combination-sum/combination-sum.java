5ab3f605d1f624e1a93fe6b9b14e40ffbfcbad48
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-02-26 16:28:09 +0530

    Time: 2 ms (85.92%) | Memory: 44.2 MB (91.89%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,20 @@
 class Solution {
-    public void getAllComb(int arr[], int target, int sum ,int i,List<List<Integer>>list, List<Integer> temp){
-        if(sum==target){
+    public void generate(int arr[],List<List<Integer>> list, List<Integer> temp, int i, int tar){
+        if(tar<0) return;
+        if(tar==0){
             list.add(new ArrayList<>(temp));
             return;
         }
-        if(sum>target) return;
-        if(i>=arr.length) return ;
-        for(int x=i; x<arr.length; x++){
-            temp.add(arr[x]);
-            getAllComb(arr,target,sum+arr[x],x,list,temp);
+        if(i>=arr.length) return;
+        for(int in=i; in<arr.length; in++){
+            temp.add(arr[in]);
+            generate(arr,list,temp,in,tar-arr[in]);
             temp.remove(temp.size()-1);
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        getAllComb(candidates,target,0,0,list,new ArrayList<>());
+        generate(candidates,list,new ArrayList<>(),0,target);
         return list;
     }
 }
