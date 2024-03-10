2b185d091abbbc3f6ab1d6ed6915f7cdcd661d6d
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-03-10 20:20:27 +0530

    Time: 36 ms (50.00%) | Memory: 61.7 MB (100.00%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -3,11 +3,9 @@ class Solution {
         Arrays.sort(happiness);
         long res= 0 ;
         int x=  0;
-        for(int i=happiness.length-1; i>=0; i--){
-            if(k==0) break;
-            k--;
-            if(happiness[i]-x>0)res+=happiness[i]-x;
-            if(happiness[i]-x==0)  continue;
+        for(int i=1; i<=k; i++){
+            if(happiness[happiness.length-i]-x>0) res+=happiness[happiness.length-i]-x;
+            if(happiness[happiness.length-i]-x==0)  continue;
             x++;
         }
         return res;
