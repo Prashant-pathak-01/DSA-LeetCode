622727b6e22f42195286f2822a9b908d77a91193
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-03-14 20:57:40 +0530

    Time: 3 ms (69.27%) | Memory: 48 MB (45.82%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int res=0 ;
-        int presum[] = new int[nums.length];
-        presum[0] = nums[0];
-        for(int i=1; i<nums.length; i++) presum[i] = presum[i-1]+nums[i];
-        for(int i=0; i<nums.length; i++){
-            for(int j=i; j<nums.length; j++){
-                if(presum[j]-presum[i]+nums[i]==goal) res++;
-            }
+        int presum[] = new int[nums.length+1];
+        presum[0] = 1;
+        int sum = 0;
+        for(int i:nums){
+            sum+=i;
+            if(sum>=goal) res+=presum[sum-goal];
+            presum[sum]++;
         }
         return res;
     }
