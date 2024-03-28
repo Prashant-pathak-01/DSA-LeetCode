c5c2d19f96e41d4aa4c419eee19d5e766ab8cedd
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-03-29 01:45:49 +0530

    Time: 66 ms (81.24%) | Memory: 61.3 MB (16.89%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -6,17 +6,12 @@ class Solution {
         int j = 0;
         int res = 0;
         while(i<nums.length){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
-            }else{
-                map.put(nums[i],map.get(nums[i])+1);
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            while(map.get(nums[i])>k){
+                map.put(nums[j],map.get(nums[j])-1);
+                j++;
             }
-                while(map.get(nums[i])>k){
-                    map.put(nums[j],map.get(nums[j])-1);
-                    j++;
-                }
-
-            res= Math.max(res,i-j+1);
+            res=Math.max(res,i-j+1);
             i++;
         }
 
