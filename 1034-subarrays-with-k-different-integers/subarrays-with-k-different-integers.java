17d30db77fae7f963796efe9bf9a16865e46f08d
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-03-31 00:19:38 +0530

    Time: 42 ms (62.36%) | Memory: 47.8 MB (12.21%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int atMost(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int i = 0, j = 0;
-        int n = nums.length;
-        int res = 0;
-        while (j < n) {
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-            while (map.size() > k && i <= j) {
-                map.put(nums[i], map.get(nums[i]) - 1);
-                if (map.get(nums[i]) == 0) map.remove(nums[i]);
+    public int subarrays(int nums[], int k){
+        Map<Integer,Integer> map=  new HashMap<>();
+        int i =0;
+        int j =0 ;
+        int res= 0 ;
+        while(j<nums.length){
+            map.merge(nums[j],1,Integer::sum);
+            while(map.size()>k && i<=j){
+                map.merge(nums[i],-1,Integer::sum);
+                if(map.get(nums[i])==0) map.remove(nums[i]);
                 i++;
             }
-            res+= (j-i+1);
+            res+=(j-i+1);
             j++;
         }
         return res;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int res = atMost(nums, k) - atMost(nums, k - 1);
-        return res;
+        return subarrays(nums,k)-subarrays(nums,k-1);
     }
 }
