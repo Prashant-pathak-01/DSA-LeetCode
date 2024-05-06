52240a1f93e62a7e7c1a6ac2117bbaa392267b53
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-05-06 17:15:21 +0530

    Time: 5 ms (40.26%) | Memory: 44.7 MB (53.43%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,22 +1,33 @@
+class Pair{
+    int a;
+    int b;
+    Pair(int d1, int d2){
+        a = d1;
+        b = d2;
+    }
+}
 class MinStack {
-    List<Integer> list;
+    Stack<Pair> stack;
+    int min;
     public MinStack() {
-        list = new ArrayList<>();
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
     }
-    
     public void push(int val) {
-        list.add(val);
+        if(stack.isEmpty()) min = val;
+        else if(val<stack.peek().b) min = val;
+        else min = stack.peek().b;
+        Pair pair = new Pair((int)val,min);
+        stack.push(pair);
     }
-    
     public void pop() {
-        list.remove(list.size()-1);
+        stack.pop();
     }
-    
     public int top() {
-        return list.get(list.size()-1);
+        return stack.peek().a;
     }
-    
     public int getMin() {
-        return Collections.min(list);
+        return stack.peek().b;
+
     }
 }
