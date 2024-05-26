37232363745bd592845cac9f78e8ede6db840126
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-05-26 23:12:59 +0530

    Time: 5 ms (75.93%) | Memory: 45.8 MB (39.53%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<numCourses; i++) list.add(new ArrayList<>());
+        int degree[] = new int[numCourses];
+        for(int x[]:prerequisites){
+            list.get(x[1]).add(x[0]);
+            degree[x[0]]++;
+        }
+        
+        int res[] = new int[numCourses];
+        int y = 0;
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) if(degree[i]==0) q.add(i);
+        while(!q.isEmpty()){
+            int course = q.poll();
+            if(y==numCourses) return new int[0];
+            res[y++] = course;
+            for(int x:list.get(course)){
+                degree[x]--;
+                if(degree[x]==0) q.add(x);
+            }
+        }
+        return (y==numCourses) ? res: new int[0];
+    }
+}
