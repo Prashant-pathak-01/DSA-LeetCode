1f898cabf10795fa3155f0ddac8e6f6e1f466da1
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-06-13 21:24:05 +0530

    Time: 1 ms (60.25%) | Memory: 40.3 MB (98.58%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -1,15 +1,16 @@
 class Solution {
     public String convertToBase7(int num) {
         if(num==0) return "0";
-        String res=  "";
+        StringBuilder res=  new StringBuilder();
         boolean flag = num<0;
         num = Math.abs(num);
         while(num>0){
             int rem = num%7;
-            res = rem+res;
+            res = res.append(rem);
             num = num/7;
         }
-        res = flag?"-"+res:""+res;
-        return res;
+        res.reverse();
+        if(flag) res.insert(0,"-");
+        return res.toString();
     }
 }
