2d67d382e76216d2d1c6626d44453a7069913b6f
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-06-18 12:18:07 +0530

    Time: 7 ms (15.48%) | Memory: 56.9 MB (40.48%) - LeetSync

--- 1659-get-the-maximum-score/get-the-maximum-score.java
+++ 1659-get-the-maximum-score/get-the-maximum-score.java
@@ -1,37 +1,31 @@
 class Solution {
-    // int MOD = (int)1e9+7;
-    public int maxSum(int[] arr, int[] brr) {
+    int MOD = (int)1e9+7;
+    public int maxSum(int[] nums1, int[] nums2) {
         int i = 0;
         int j = 0;
         long res = 0;
-
-        long sum1 = 0;
-        long sum2 = 0;
-        while(i < arr.length && j < brr.length){
-            if(arr[i] < brr[j]){
-                sum1 += arr[i];
-                i++;
+        while (i <= nums1.length && j <= nums2.length) {
+            long sum1 = 0;
+            long sum2 = 0;
+            while (i < nums1.length && j < nums2.length && nums1[i] != nums2[j]) {
+                if (nums1[i] < nums2[j])
+                    sum1 = (sum1+nums1[i++])%MOD;
+                else
+                    sum2 = (sum2+nums2[j++])%MOD;
             }
-            else if(arr[i] > brr[j]){
-                sum2 += brr[j++];
+            while (i == nums1.length && j < nums2.length) {
+                sum2 = (sum2+nums2[j++]);
             }
-            else{
-                res += Math.max(sum1 , sum2) + arr[i];
-                i++;
-                j++;
-                sum1 = 0;
-                sum2 = 0;
+            while (j == nums2.length && i < nums1.length) {
+                sum1 = (sum1+nums1[i++]);
             }
+            res = (res+Math.max(sum1, sum2));
+            if(i<nums1.length && j<nums2.length) res = (res+nums1[i]);
+            i++;
+            j++;
+            
         }
-
-        while(i < arr.length){
-            sum1 += arr[i++];
-        }
-        while(j < brr.length){
-            sum2 += brr[j++];
-        }
-
-        res += Math.max(sum1 , sum2);
-        return (int) (res % (1e9 + 7)) ;
+        
+        return (int)(res %(1e9+7));
     }
 }
