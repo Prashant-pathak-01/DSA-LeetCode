c1a41de8ee8022fdfab5957479f1e2f8db3d06f1
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-06-19 12:09:18 +0530

    Time: 248 ms (5.08%) | Memory: 54.8 MB (5.56%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,29 +1,29 @@
 class Solution {
-    int mod = (int)1e9+7;
-    public long getMax(int i, int j, int n, int indx,long [][][]dp){
-        if(i>=4 || i<0 || j>=3 || j<0 || (i==3 && j==0) || (i==3 && j==2)) return 0;
-        if(indx==n) return 1;
-        if(dp[i][j][indx]!=-1) return dp[i][j][indx];
-        long a = getMax(i+2,j-1,n,indx+1,dp)%mod;
-        long b = getMax(i+2,j+1,n,indx+1,dp)%mod;
-        long c = getMax(i-2,j+1,n,indx+1,dp)%mod;
-        long d = getMax(i-2,j-1,n,indx+1,dp)%mod;
-        long e = getMax(i-1,j+2,n,indx+1,dp)%mod;
-        long f = getMax(i+1,j+2,n,indx+1,dp)%mod;
-        long g = getMax(i-1,j-2,n,indx+1,dp)%mod;
-        long h = getMax(i+1,j-2,n,indx+1,dp)%mod;
-        return dp[i][j][indx] =  (a+b+c+d+e+f+g+h)%mod;
+    int MOD = (int)1e9+7;
+    public int solve(int n, int i, int j,int dp[][][]){
+        if((i==3 && j==0) ||(i==3 && j==2)) return 0;
+        if(i>=4 || j>=3 || i<0 || j<0) return 0;
+        if(dp[n][i][j]!=-1) return dp[n][i][j];
+        if(n==0) return 1;
 
+        int res =0 ;
+        int l[] = {2,2,1,-1,-2,-2,1,-1};
+        int r[] = {1,-1,2,2,1,-1,-2,-2};
+
+        for(int x=0; x<8; x++){
+            res=(res+solve(n-1,i+l[x],j+r[x],dp))%MOD;
+        }
+        return dp[n][i][j]=res%MOD;
     }
     public int knightDialer(int n) {
-        int res = 0;
-        long dp[][][] = new long[4][3][n+1];
-        for(long [][]i:dp) for(long j[]:i) Arrays.fill(j,-1);
-        for(int i=0; i<=3; i++){
-            for(int j=0; j<=2; j++){
-                res=(res+(int)getMax(i,j,n,1,dp))%mod;
+        int res= 0;
+        int dp[][][] = new int[n][4][4];
+        for(int i[][]:dp) for(int j[]:i) Arrays.fill(j,-1);
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                res=(res+solve(n-1,i,j,dp))%MOD;
             }
         }
-        return res%mod;
+        return res%MOD;
     }
 }
