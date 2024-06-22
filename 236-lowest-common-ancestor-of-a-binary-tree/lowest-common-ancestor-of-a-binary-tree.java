39c6367e94421c706a1f42f53f71ae576cd62a9f
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-06-22 11:18:32 +0530

    Time: 6 ms (100.00%) | Memory: 44.4 MB (74.21%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,25 +1,11 @@
 
 class Solution {
-    TreeNode res;
-    public boolean contains(TreeNode root, TreeNode p, TreeNode q, int arr[]){
-        if(root==null) return false;
-        if(root==p) arr[0] = 1;
-        if(root==q) arr[1] = 1;
-        if(arr[0]==1 && arr[1]==1) return true;
-        return contains(root.left,p,q,arr) || contains(root.right,p,q,arr);
-    }
-    public void solve(TreeNode root,TreeNode p, TreeNode q){
-        if(root==null) return;
-        res = root;
-        if(contains(root.left,p,q,new int[2]) ) {
-            solve(root.left,p,q);
-        }else if(contains(root.right,p,q,new int[2])){
-            solve(root.right,p,q);
-        }else return;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        res = root;
-        solve(root,p,q);
-        return res;
+        if(root==null) return null;
+        if(root==p) return root;
+        if(root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        return left==null?right:right==null?left:root;
     }
 }
