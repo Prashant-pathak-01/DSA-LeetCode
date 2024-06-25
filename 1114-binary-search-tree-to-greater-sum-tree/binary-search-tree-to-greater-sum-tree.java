e18d86aa0fe733c72ccdd39b673236abcc0a7b3c
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-06-25 21:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (87.31%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -1,31 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int func(TreeNode root, int sum){
-        if(root==null) return 0;
-        if(root.right!=null) sum=func(root.right,sum);
-        int a = root.val;
-        sum+=a;
-        root.val = sum;
-        if(root.left!=null) sum=func(root.left,sum);
-        return sum;
-
-    }
+    int sum =0;
     public TreeNode bstToGst(TreeNode root) {
-        func(root,0);
+        if(root==null) return root;
+        bstToGst(root.right);
+        sum+=root.val;
+        root.val = sum;
+        bstToGst(root.left);
         return root;
     }
 }
