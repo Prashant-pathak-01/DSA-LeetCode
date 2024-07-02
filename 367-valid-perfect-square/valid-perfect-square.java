7060ef03697a684a1816a541e8fac70030187552
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-02 21:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (56.24%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        int l =1;
+        int r = num/2;
+        while(l<=r){
+            long mid = (l+r)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid>num) r = (int)mid-1;
+            else l = (int)mid+1; 
+        }
+        return false;
+    }
+}
