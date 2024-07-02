7f258cd7b519e250513d532354df0fafb4b0dd09
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-02 15:24:46 +0530

    Time: 1 ms (73.86%) | Memory: 44.6 MB (39.09%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        if(root==null) return 0;
-        int res = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int s = q.size();
-            for(int i=0; i<s; i++){
-                if(q.peek().left!=null) q.add(q.peek().left);
-                if(q.peek().right!=null) q.add(q.peek().right);
-                if(i==0){
-                    res = q.peek().val;
-                }
-                q.poll();
-            }
-        }
-        return res;
+    public long solve(long depth, TreeNode root){
+        if(root==null) return Long.MIN_VALUE;
+        if(depth==1) return root.val;
+        long left = solve(depth-1,root.left);
+        if(left!=Long.MIN_VALUE) return left;
+        long right = solve(depth-1,root.right);
+        return right;
     }
+    public int depth(TreeNode root){
+        if(root==null) return 0;
+        return 1+Math.max(depth(root.left),depth(root.right));
+    }
+    public int findBottomLeftValue(TreeNode root) {
+        int depth = depth(root);
+        return (int)solve(depth,root);
 
+    }
 }
