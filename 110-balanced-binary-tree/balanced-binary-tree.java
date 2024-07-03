9b444a3dd41eb25bf2f452ca2d3a48c93785b754
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-03 10:46:21 +0530

    Time: 2 ms (5.83%) | Memory: 44.6 MB (16.47%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,32 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    Map<TreeNode,Integer> map = new HashMap<>();
     public int height(TreeNode root){
         if(root==null) return 0;
-        return 1+Math.max(height(root.left),height(root.right));
+        int res= 1+Math.max(height(root.left),height(root.right));
+        map.put(root,res);
+        return res;
     }
     public boolean solve(TreeNode root){
         if(root==null) return true;
-        int left = height(root.left);
-        int right = height(root.right);
+        int left = map.get(root.left);
+        int right = map.get(root.right);
         if(Math.abs(left-right)>1) return false;
         return solve(root.left) && solve(root.right); 
 
     }
     public boolean isBalanced(TreeNode root) {
+        height(root);
+        map.put(null,0);
         return solve(root);
     }
 }
