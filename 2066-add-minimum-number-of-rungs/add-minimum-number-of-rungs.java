1c32daa0a057795c094121cdc8be6e379e58fa4f
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-08 11:08:02 +0530

    Time: 2 ms (28.03%) | Memory: 57.1 MB (87.26%) - LeetSync

---
+++ 2066-add-minimum-number-of-rungs/add-minimum-number-of-rungs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int addRungs(int[] rungs, int dist) {
+        int res= 0;
+        int prev =0 ;
+        for(int i=0; i<rungs.length; i++){
+            res+=(rungs[i]-prev-1)/dist;
+            prev = rungs[i];
+        }
+        return res;
+    }
+}
