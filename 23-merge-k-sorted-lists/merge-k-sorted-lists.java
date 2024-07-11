bf4d6555efe75199088f94c1554b1fd2f40ddbd0
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-11 10:46:52 +0530

    Time: 65 ms (17.90%) | Memory: 45.9 MB (5.27%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,52 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode sort(ListNode l1, ListNode l2){
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        ListNode res = new ListNode(0);
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode res  = new ListNode();
         ListNode temp = res;
-
         while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                res.next = new ListNode(l1.val);
-                l1 = l1.next;
-            } else{
+            if(l1.val>l2.val){
                 res.next = new ListNode(l2.val);
                 l2 = l2.next;
+            }else{
+                res.next = new ListNode(l1.val);
+                l1 = l1.next;
             }
             res = res.next;
-
         }
-        while(l1!=null){
-            res.next = new ListNode(l1.val);
-            l1 = l1.next;
-            res = res.next;
-        }
-        while(l2!=null){
-            res.next = new ListNode(l2.val);
-            l2 = l2.next;
-            res = res.next;
-        }
-
+        if(l1!=null) res.next = l1;
+        if(l2!=null) res.next = l2;
         return temp.next;
     }
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length==0) return null;
-        if(lists.length==1) return lists[0];
         ListNode res = lists[0];
         for(int i=1; i<lists.length; i++){
-            res = sort(res,lists[i]);
+            res = merge(res,lists[i]);
         }
         return res;
     }
-
 }
