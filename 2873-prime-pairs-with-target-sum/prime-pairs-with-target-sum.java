b2639170d4c2884eaf883e38b9207507a73d7931
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-15 16:11:00 +0530

    Time: 227 ms (33.09%) | Memory: 55 MB (35.32%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -3,24 +3,23 @@ class Solution {
         List<List<Integer>> list=new ArrayList<>();
         boolean[] ans=prime(n+1);
         for(int i=2;i<=n/2;i++){
-            if(!ans[i]){
-                if(!ans[n-i]){
-                    list.add(new ArrayList<>(Arrays.asList(i,n-i)));
-                }
+            if(!ans[i] && !ans[n-i]){
+                list.add(new ArrayList<>(Arrays.asList(i,n-i)));
             }
         }
         return list;
     }
+
     public static boolean[] prime(int n) {
-	boolean[]arr=new boolean[n+1];
-	arr[0]=arr[1]=true;
-	for(int i=2;i<arr.length;i++) {
-		if(!arr[i]) {
-			for(int k=2;k*i<arr.length;k++) {
-				arr[i*k]=true;
-			}
+	    boolean[]arr=new boolean[n+1];
+	    arr[0]=arr[1]=true;
+	    for(int i=2;i<arr.length;i++) {
+		    if(!arr[i]) {
+			    for(int k=2;k*i<arr.length;k++) {
+			    	arr[i*k]=true;
+		    	}
 		}
-	}
-	return arr;
+	    }
+	    return arr;
 	}
 }
