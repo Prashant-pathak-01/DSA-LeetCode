b416103cc628968a65e7db8886522dd87dcb5c8a
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-18 11:54:08 +0530

    Time: 12 ms (71.59%) | Memory: 49.1 MB (11.47%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int []>> list = new ArrayList<>();
+        for(int i=0; i<=n; i++) list.add(new ArrayList<>());
+        for(int i=0; i<times.length; i++){
+            list.get(times[i][0]).add(new int[]{times[i][1],times[i][2]});
+        }
+        int finalTime[] = new int[n+1];
+        Arrays.fill(finalTime,Integer.MAX_VALUE);
+        Queue<int []> q = new LinkedList<>();
+        q.add(new int[]{k,0});
+        finalTime[k] = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int temp[] = q.poll();
+                for(int[] x:list.get(temp[0])){
+                    int min = temp[1]+x[1];
+                    if(min<finalTime[x[0]]){
+                        finalTime[x[0]] = min;
+                        q.add(new int[]{x[0],min});
+                    }
+                }
+            }
+        }
+        int res= -1;
+        System.out.println(Arrays.toString(finalTime));
+        for(int x=1; x<finalTime.length; x++){
+            if(finalTime[x]==Integer.MAX_VALUE) return -1;
+            if(x!=k) res = Math.max(res,finalTime[x]);
+        }
+        return res;
+    }
+}
