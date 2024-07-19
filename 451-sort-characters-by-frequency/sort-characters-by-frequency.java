7d8d5aa48746cab51e5f1ba05303cc2719e4b048
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-07-19 11:03:16 +0530

    Time: 15 ms (53.83%) | Memory: 45.6 MB (45.56%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,23 +1,15 @@
 class Solution {
     public String frequencySort(String s) {
-        int arr[][] = new int[128][2];
-        for(int i=0; i<128;i++){
-            arr[i][0]  = i;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(char key:map.keySet()){
+            pq.add(new int[]{key-'0',map.get(key)});
         }
-        for(int i=0; i<s.length(); i++){
-            if(Character.isLowerCase(s.charAt(i))) arr[(int)(s.charAt(i)-'a')][1]++;
-            else if (Character.isUpperCase(s.charAt(i))) arr[(int)(s.charAt(i)-'A')+26][1]++;
-            else arr[(int)(s.charAt(i)-'0')+52][1]++;
-        }
-        Arrays.sort(arr, Comparator.comparingInt((int[] a) -> a[1]).reversed());
         StringBuilder res = new StringBuilder();
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i][1]==0) break;
-            for(int k=0; k<arr[i][1]; k++){
-                if(arr[i][0]<26) res.append((char)('a'+arr[i][0]));
-                else if(arr[i][0]<52) res.append((char)('A'+arr[i][0]-26));
-                else res.append((char)('0'+arr[i][0]-52));
-            }
+        while(!pq.isEmpty()){
+            int temp[] = pq.poll(); 
+            for(int i=0; i<temp[1]; i++) res.append((char)(temp[0]+'0'));
         }
         return res.toString();
     }
