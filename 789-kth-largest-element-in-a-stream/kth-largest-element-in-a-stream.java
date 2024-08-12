9d291cbdfe69c3a44d4a9bcf42172bd3c4bef090
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-08-12 23:32:21 +0530

    Time: 21 ms (7.90%) | Memory: 47 MB (33.32%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,21 +1,21 @@
 class KthLargest {
-    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+    int max = Integer.MAX_VALUE;
     PriorityQueue<Integer> storage = new PriorityQueue<>((a,b)->a-b);
     public KthLargest(int k, int[] nums) {
         Arrays.sort(nums);
         if(k<=nums.length){
             for(int i=nums.length-1; i>nums.length-k; i--) storage.add(nums[i]);
-            for(int i=nums.length-k; i>=0; i--) pq.add(nums[i]);
+            max = nums[nums.length-k];
         }else{
             for(int i=0; i<nums.length; i++) storage.add(nums[i]);
         }
     }
     
     public int add(int val) {
-        if(!pq.isEmpty() && val<=pq.peek()) return pq.peek();
+        if(max!=Integer.MAX_VALUE && val<=max) return max;
         storage.add(val);
         int temp = storage.poll();
-        pq.add(temp);
+        max = temp;
         return temp;
     }
 }
