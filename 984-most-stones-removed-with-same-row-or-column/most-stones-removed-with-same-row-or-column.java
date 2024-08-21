63a0cd077da4e312a9e092c7fa50df15490283c8
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-08-21 16:52:41 +0530

    Time: 174 ms (5.59%) | Memory: 44.7 MB (84.54%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,20 +1,21 @@
 class Solution {
-    public void dfs(int i, int stones[][], boolean visited[]){
+    boolean visited[];
+    public void dfs(int i, int stones[][]){
         for(int x =0; x<stones.length; x++){
             if(!visited[x] && i!=x && (stones[i][0]==stones[x][0] || stones[i][1]==stones[x][1])){
                 visited[x] = true;
-                dfs(x,stones,visited);
+                dfs(x,stones);
             }
         }
     }
     public int removeStones(int[][] stones) {
-        boolean visited[] = new boolean[stones.length];
+        visited = new boolean[stones.length];
         int count = 0;
         for(int i=0; i<stones.length; i++){
             if(!visited[i]){
                 count++;
                 visited[i] = true;
-                dfs(i,stones,visited);
+                dfs(i,stones);
             }
         }
         return stones.length-count;
