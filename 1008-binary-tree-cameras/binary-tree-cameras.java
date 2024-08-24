99bb18c5f363ce359e65b6d31280524038d679a4
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-08-25 01:20:54 +0530

    Time: 21 ms (14.02%) | Memory: 44.5 MB (5.67%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count =0;
+    public int solve(TreeNode root){
+        if(root==null) return -1;
+        if(root.left==null && root.right==null) return 0;
+        int left = solve(root.left);
+        int right = solve(root.right);
+        System.out.println(left+" "+right);
+        if(left==0 || right==0){
+            count++;
+            return 1;
+        }
+        if(left==1 || right==1) return 2;
+        return 0;
+
+    }
+    public int minCameraCover(TreeNode root) {
+        return (solve(root)==0?1:0)+count;
+    }
+}
