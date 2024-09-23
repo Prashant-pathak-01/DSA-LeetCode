b72efb6fd1e8be0e44f0cf0e5f238a77cca9e45b
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-09-23 22:39:08 +0530

    Time: 15 ms (5.03%) | Memory: 45.3 MB (5.52%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void generate(String s,List<String> list, int i, int count, String res ){
+        if(count>4) return;
+        if(i==s.length()){
+            if(count==4 && isValid(res.substring(1))) list.add(res.substring(1));
+            return;
+        }
+        for(int x=i; x<s.length(); x++){
+            generate(s,list,x+1,count+1,res+"."+s.substring(i,x+1));
+        }
+    }
+    public boolean isValid(String res){
+        String arr[] = res.split("\\.");
+        for(String x:arr){
+            if(x.length()==1) continue;
+            if(x.charAt(0)!='0' && Long.parseLong(x)>=0 && Long.parseLong(x)<=255 ){
+                continue;
+            }else return false;
+        }
+        return true;
+    }
+    public List<String> restoreIpAddresses(String s) {
+        List<String> list = new ArrayList<>();
+        generate(s,list,0,0,"");
+        return list;
+    }
+}
