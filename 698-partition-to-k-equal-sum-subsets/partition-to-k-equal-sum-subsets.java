594523930bd3519f086cea95b191139d444a4497
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-10-12 11:43:06 +0530

    Time: 1245 ms (41.72%) | Memory: 41.5 MB (30.14%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -15,6 +15,11 @@ class Solution {
         for(int i:nums) sum+=i;
         if(sum%k!=0) return false;
         Arrays.sort(nums);
+        for(int i=0; i<nums.length/2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length-1-i];
+            nums[nums.length-1-i] = temp;    
+        }
         boolean visited[] = new boolean[nums.length];
         return solve(nums,visited,0,k,0,sum/k);
     }
