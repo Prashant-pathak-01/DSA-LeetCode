f2140092190f8b79c81ab1eeb7a254d109e4796e
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-10-22 15:53:50 +0530

    Time: 632 ms (24.65%) | Memory: 60.7 MB (21.93%) - LeetSync

--- 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.java
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.java
@@ -16,23 +16,16 @@ class Solution {
         return sieve;
     }
     public int minOperations(int[] nums) {
-        int[] seive= sieveOfEratosthenes((int)1e6);
+        int max = 0;
+        for(int i:nums) max = Math.max(max,i);
+        int[] seive= sieveOfEratosthenes(max);
         int res= 0;
         for(int i=nums.length-2; i>=0; i--){
-            if(nums[i]>nums[i+1]){
-                if(seive[nums[i]]==1 || seive[nums[i]]==nums[i]){
-                    return -1;
-                }else{
-                    int temp = nums[i]/seive[nums[i]];
-                    while(nums[i]>nums[i+1]) {
-                        if(seive[nums[i]]==1 || seive[nums[i]]==nums[i]){
-                    return -1;
-                }
-                        nums[i]/=temp;
-                        res++;
-                        temp = nums[i]/seive[nums[i]];
-                    }
-                }
+            while(nums[i]>nums[i+1]) {
+                int temp = nums[i]/seive[nums[i]];
+                if(seive[nums[i]]==1 || seive[nums[i]]==nums[i]) return -1;
+                    nums[i]/=temp;
+                    res++;
             }
         }
         return res;
