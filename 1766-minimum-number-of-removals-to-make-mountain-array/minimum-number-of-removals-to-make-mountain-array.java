98357bdb9c33b3e5ac240b9da0acd59aee47dd26
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-10-30 18:15:52 +0530

    Time: 45 ms (11.43%) | Memory: 44.2 MB (62.24%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
@@ -1,29 +1,21 @@
-public class Solution {
+class Solution {
     public int minimumMountainRemovals(int[] nums) {
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        Arrays.fill(left,1);
-        Arrays.fill(right,1);
-        for (int i=0; i<nums.length; i++) {
-            for (int j=0; j<i; j++) {
-                if (nums[i]>nums[j]) {
-                    left[i] = Math.max(left[i],left[j]+1);
-                }
+        int l[] = new int[nums.length];
+        int r[] = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i]>nums[j]) l[i] = Math.max(l[i],l[j]+1);
             }
         }
-        for (int i = nums.length-1; i>=0; i--) {
-            for (int j = nums.length-1; j>i; j--) {
-                if (nums[i] >nums[j]) {
-                    right[i] = Math.max(right[i], right[j] + 1);
-                }
+        for(int i=nums.length-1; i>=0; i--){
+            for(int j=nums.length-1; j>i; j--){
+                if(nums[i]>nums[j]) r[i] = Math.max(r[i],r[j]+1);
             }
         }
-        int ans = 0;
-        for (int i = 0; i <nums.length; i++) {
-            if (left[i] != 1 && right[i] != 1) {
-                ans = Math.max(ans, left[i] + right[i]);
-            }
+        int res = 0;
+        for(int i=1; i<nums.length-1; i++) {
+            if(l[i]>0 && r[i]>0) res = Math.max(res,l[i]+r[i]);
         }
-        return nums.length- ans + 1;
+        return nums.length-res-1;
     }
 }
