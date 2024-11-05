0fe0e8769678439fbfa5f9192c0447060efcce24
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-11-05 10:34:28 +0530

    Time: 11 ms (5.54%) | Memory: 44.9 MB (53.34%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,30 +1,36 @@
 class Solution {
-    int [][] list;
-    public boolean bfs(int node, int visited[]){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        visited[node] = 1;
-        while(!q.isEmpty()){
-            int temp = q.poll();
-            int color = visited[temp]==1?-1:1;
-            for(int x:list[temp]){
-                if(visited[x]==0) {
-                    q.add(x);
-                    visited[x] = color;
-                }
-                else if(visited[x]==visited[temp]) return false;
-                
-            }
-            
-        }
-        return true;
-    }
     public boolean isBipartite(int[][] graph) {
-        list = graph;
-        int visited[] = new int[graph.length];
-        for(int i=0; i<graph.length; i++){
-            if(visited[i]==0){
-                if(!bfs(i,visited)) return false;
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[graph.length];
+        for (int x = 0; x < graph.length; x++) {
+            if (!visited[x]) {
+                q.add(x);
+                l1.add(x);
+                while (!q.isEmpty()) {
+                    int temp = q.poll();
+                    visited[temp] = true;
+                    if (l1.contains(temp)) {
+                        for (int i = 0; i < graph[temp].length; i++) {
+                            if (l1.contains(graph[temp][i]))
+                                return false;
+                            if (!l2.contains(graph[temp][i])) {
+                                l2.add(graph[temp][i]);
+                                q.add(graph[temp][i]);
+                            }
+                        }
+                    } else if (l2.contains(temp)) {
+                        for (int i = 0; i < graph[temp].length; i++) {
+                            if (l2.contains(graph[temp][i]))
+                                return false;
+                            if (!l1.contains(graph[temp][i])) {
+                                l1.add(graph[temp][i]);
+                                q.add(graph[temp][i]);
+                            }
+                        }
+                    }
+                }
             }
         }
         return true;
