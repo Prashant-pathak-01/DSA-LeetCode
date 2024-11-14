5721f64f9315a39a4856076ebc3bc61c18a6ebcf
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-11-14 22:07:36 +0530

    Time: 1 ms (99.38%) | Memory: 42.2 MB (41.00%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int sum =0;
+        for(int i=0; i<num.length(); i++){
+            if(i%2==0) sum+=(num.charAt(i)-'0');
+            else sum-=(num.charAt(i)-'0');
+        }
+        return sum==0;
+    }
+}
