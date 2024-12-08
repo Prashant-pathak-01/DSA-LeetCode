43880c2a6fb82ba042d2c6f38eff2f26800140be
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-12-08 18:12:18 +0530

    Time: 2567 ms (5.06%) | Memory: 61.3 MB (16.10%) - LeetSync

---
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getLargestOutlier(int[] nums) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        //Arrays.sort(nums);
+        int res = Integer.MIN_VALUE;
+        for(int i:nums){
+            sum+=i;
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        for(int i=nums.length-1; i>=0; i--) {
+            System.out.println((sum-nums[i])+" "+nums[i]);
+            if((sum-nums[i])%2==0 && map.containsKey((sum-nums[i])/2)) {
+                if(nums[i]==(sum-nums[i])/2) {
+                    if(map.get((sum-nums[i])/2)>1) res = Math.max(res,nums[i]);
+                }
+                else {
+                    
+                    res = Math.max(res,nums[i]);
+                }
+            }
+        }
+        return res;
+    }
+}
