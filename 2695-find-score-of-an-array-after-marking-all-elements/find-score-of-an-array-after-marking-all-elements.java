edb4d09d2009d0d0160a0c513ba377997de33bb2
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-12-13 12:17:13 +0530

    Time: 171 ms (53.92%) | Memory: 57.3 MB (81.02%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -10,8 +10,6 @@ class Solution {
     public long findScore(int[] nums) {
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.val==b.val?a.index-b.index:a.val-b.val);
         for(int i=0; i<nums.length; i++) pq.add(new Pair(i,nums[i]));
-        Map<Integer,Pair> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
         long res = 0;
         while(!pq.isEmpty()){
             Pair temp = pq.poll();
