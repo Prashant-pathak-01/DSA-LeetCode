1b3d54240b8e4d7690788b4382e8099871c3f531
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2024-12-13 11:27:19 +0530

    Time: 11 ms (23.32%) | Memory: 41.9 MB (32.16%) - LeetSync

--- 553-optimal-division/optimal-division.java
+++ 553-optimal-division/optimal-division.java
@@ -2,10 +2,10 @@ class Solution {
     public String optimalDivision(int[] nums) {
         if(nums.length==1) return nums[0]+"";
         if(nums.length==2) return nums[0]+"/"+nums[1];
-        String str = "";
+        StringBuilder str = new StringBuilder();
         for(int i=1; i<nums.length; i++) {
-            str = str+nums[i];
-            if(i<nums.length-1) str+="/";
+            str = str.append(nums[i]);
+            if(i<nums.length-1) str.append("/");
         }
         return nums[0]+"/("+str+")";
     }
