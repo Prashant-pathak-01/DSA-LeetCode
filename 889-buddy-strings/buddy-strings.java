2f352c072a8ecbe5ee0065a1426933d5f0473a8a
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-01-09 22:56:19 +0530

    Time: 5 ms (20.18%) | Memory: 42.9 MB (13.57%) - LeetSync

--- 889-buddy-strings/buddy-strings.java
+++ 889-buddy-strings/buddy-strings.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
         if(s.length()!=goal.length()) return false;
+        int ch[] = new int[26];
         int count =0;
         int x = -1;
+        boolean flag = false;
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)!=goal.charAt(i)){
                 count++;
@@ -10,13 +12,11 @@ class Solution {
                     if(s.charAt(x)!=goal.charAt(i) || s.charAt(i)!=goal.charAt(x) ) return false;
                 }else x = i;
             }
+            ch[s.charAt(i)-'a']++;
+            if(ch[s.charAt(i)-'a']==2) flag = true;
         }
         if(count==2) return true;
         else if(count==1 || count>2) return false;
-
-        Map<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++) map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        for(char key:map.keySet()) if(map.get(key)>=2) return true;
-        return false;
+        else return flag;
     }
 }
