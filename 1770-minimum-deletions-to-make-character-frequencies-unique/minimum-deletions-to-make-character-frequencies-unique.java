9ddd18d63ea6136e14fb8e1f75ad4cfc94bf21a4
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-01-10 15:14:18 +0530

    Time: 8 ms (76.94%) | Memory: 45.1 MB (91.61%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,21 +1,19 @@
 class Solution {
     public int minDeletions(String s) {
         int arr[] = new int[26];
-        for(int i=0; i<s.length(); i++){
-            arr[s.charAt(i)-'a']++;
-        }
-        
-        int res = 0;
-        boolean bool[] = new boolean[s.length()+1];
+        for(int i=0; i<s.length(); i++) arr[s.charAt(i)-'a']++; 
+        Arrays.sort(arr);
+        int res =0;
+        int prev =Integer.MAX_VALUE;
+        //System.out.println(Arrays.toString(arr));
+        for(int x=arr.length-1; x>=0; x--){
+            if(arr[x]>0 && prev<arr[x]){
+                res+=Math.min(arr[x],Math.abs(arr[x]-prev));
+            }
+            prev = Math.min(arr[x]-1,prev-1);
+            //System.out.println(res);
 
-        for(int i= 0; i<26; i++){
-            while(bool[arr[i]] && arr[i]>0){
-                res++;
-                arr[i]--;
-            }  
-            bool[arr[i]] = true;  
         }
         return res;
-
     }
 }
