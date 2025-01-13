b4e04691ff3c24088e4e7781f63248b44ce80722
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-01-13 21:45:21 +0530

    Time: 12 ms (22.61%) | Memory: 45.8 MB (55.68%) - LeetSync

---
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int res= 0;
+        for(int i=nums.length-1; i>0; i--){
+            res+=nums[i]-nums[0];
+        }
+        return res;
+    }
+}
