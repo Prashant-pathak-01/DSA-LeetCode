00cc1de87a07c0e44e48a8ab149531d408af969b
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-03-11 01:45:57 +0530

    Time: 351 ms (64.23%) | Memory: 105.6 MB (5.42%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
@@ -10,16 +10,16 @@ class Solution {
         }
     }
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Double.compare(a.avg,b.avg));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Double.compare(b.avg,a.avg));
         for(int i=0; i<classes.length; i++){
-            double diff = (double)(classes[i][0])/(classes[i][1])-(double)(classes[i][0]+1)/(classes[i][1]+1);
+            double diff = (double)(classes[i][0]+1)/(classes[i][1]+1)-(double)(classes[i][0])/(classes[i][1]);
             pq.add(new Pair(classes[i][1],classes[i][0],diff));
         }
         while(extraStudents-->0){
             Pair temp = pq.poll();
             temp.total++;
             temp.pass++;
-            double diff = (double)(temp.pass)/(temp.total)-(double)(temp.pass+1)/(temp.total+1);
+            double diff = (double)(temp.pass+1)/(temp.total+1)-(double)(temp.pass)/(temp.total);
             pq.add(new Pair(temp.total,temp.pass,diff));
         }
         double res= 0;
