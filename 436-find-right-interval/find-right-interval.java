20626c54f4791128ec9e988226aa2e7bf464af94
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-03-11 23:48:15 +0530

    Time: 14 ms (55.73%) | Memory: 49.7 MB (26.28%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        int arr[][] = new int[intervals.length][3];
+        for(int i=0; i<intervals.length; i++){
+            arr[i][0] = intervals[i][0];
+            arr[i][1] = intervals[i][1];
+            arr[i][2] = i;
+        }
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int res[] = new int[intervals.length];
+        for(int i=0; i<arr.length; i++){
+            int tar = arr[i][1];
+            int l = 0;
+            int r = arr.length-1;
+            int mid;
+            int ans = -1;
+            while(l<=r){
+                mid = (l+r)/2;
+                if(arr[i][2]==1){
+                    System.out.println(mid);
+                }
+                if(arr[mid][0]>=tar){
+                    ans = arr[mid][2];
+                    r = mid-1;
+                }else l = mid+1;
+            }
+            res[arr[i][2]] = ans;
+        }
+        return res;
+    }
+}
