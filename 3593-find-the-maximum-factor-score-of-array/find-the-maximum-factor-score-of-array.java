0b95d40f52556e5c0cdbbf3c080e1e73958a70ca
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2025-06-26 02:31:42 +0530

    Time: 38 ms (6.56%) | Memory: 44.6 MB (16.39%) - LeetSync

---
+++ 3593-find-the-maximum-factor-score-of-array/find-the-maximum-factor-score-of-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public long hcf(long a, long b) {
+        while(b != 0) {
+            long temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+    public long lcm(long a, long b) {
+    return (a*b)/hcf(a,b);
+    }
+    public long maxScore(int[] nums) {
+        long hcfPre[]= new long[nums.length];
+        long prev =nums[0];
+        for(int i=0; i<nums.length; i++){
+            hcfPre[i] = hcf(nums[i],prev);
+            prev = hcfPre[i];
+        }
+        long hcfPost[]= new long[nums.length];
+        prev =nums[nums.length-1];
+        for(int i=nums.length-1; i>=0; i--){
+            hcfPost[i] = hcf(nums[i],prev);
+            prev = hcfPost[i];
+        }
+        long lcmPre[]= new long[nums.length];
+        prev =1;
+        for(int i=0; i<nums.length; i++){
+            lcmPre[i] = lcm(nums[i],prev);
+            prev = lcmPre[i];
+        }
+        long lcmPost[]= new long[nums.length];
+        prev =1;
+        for(int i=nums.length-1; i>=0; i--){
+            lcmPost[i] = lcm(nums[i],prev);
+            prev = lcmPost[i];
+        }
+        // System.out.println(Arrays.toString(hcfPre));
+        long res =lcmPost[0]*hcfPost[0];
+        for(int i=0; i<nums.length; i++){
+            long lcm = lcm(i>0?lcmPre[i-1]:1,i<nums.length-1?lcmPost[i+1]:1);
+            long hcf = nums.length==1?nums[i]:(i>0 && i<nums.length-1)?hcf(hcfPre[i-1],hcfPost[i+1]):i==0?hcfPost[i+1]:hcfPre[i-1];
+            System.out.println(lcm+" "+hcf);
+            res = Math.max(res,hcf*lcm);
+        }
+
+        return res;
+    }
+}
